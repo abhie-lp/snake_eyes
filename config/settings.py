1a3a8d6a2493b2819842b6d950f77ed7b233ca13
Abhishek Tiwari <abhie.lp@gmail.com>
2021-07-03 23:34:22 +0530

    Use gmail for SMTP

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ SERVER_NAME = "localhost:8000"
 SECRET_KEY = "this is a big secret key. Don't share it"
 
 MAIL_DEFAULT_SENDER = "contact@snake_eyes.host"
-MAIL_SERVER = "smtp-relay.sendinblue.com"
+MAIL_SERVER = "smtp.gmail.com"
 MAIL_PORT = 587
 MAIL_USE_TLS = True
 MAIL_USE_SSL = False
