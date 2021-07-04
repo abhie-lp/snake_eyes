bf6c47a6f866a96a885bacd902b6d66671aca08c
Abhishek Tiwari <abhie.lp@gmail.com>
2021-07-04 12:28:47 +0530

    Send email by rendering template

--- snake_eyes/utils/email.py
+++ snake_eyes/utils/email.py
@@ -4,17 +4,20 @@ from smtplib import SMTP_SSL
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-from flask import current_app as app
+from flask import current_app as app, render_template
 
 
-def send_mail(subject: str, to: str, via: str, content: str) -> None:
+def send_mail(
+        subject: str, to: str, via: str, template: str, ctx: dict
+) -> None:
     """Send mail to the user"""
     msg = MIMEMultipart("alternative")
     msg["To"] = to
     msg["From"] = via
     msg["Subject"] = subject
     msg.preamble = "Your browser does not support mail format."
-    content = MIMEText(content, "html")
+    content = MIMEText(render_template(template, **ctx),
+                       "html" if template.rsplit(".", 1)[1] == "html" else "plain")
     msg.attach(content)
 
     with SMTP_SSL(app.config["MAIL_SERVER"]) as smtp:
